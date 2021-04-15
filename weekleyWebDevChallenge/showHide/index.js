063274d64f1c13146ef765fbbbd074f0d895d4e4
brforry <67486262+brforry@users.noreply.github.com>
2021-04-15 15:09:39 -0600

    more learning

--- weekleyWebDevChallenge/showHide/index.js
+++ weekleyWebDevChallenge/showHide/index.js
@@ -1,23 +1,20 @@
 function toggleVisibility() {
     // get the emojis
-    var emojis = document.getElementById('emojis');
+    const emojis = document.getElementById('emojis');
 
-    // get the current value of the clock's display property
-    var displaySetting = emojis.style.display;
-
-    // also get the clock button, so we can change what it says
+    // also get the button, so we can change what it says
     var showHideButton = document.getElementById('show-hide-button');
 
-    // now toggle the clock and the button text, depending on current state
-    if (displaySetting == 'block') {
+    // now toggle the emoji and the button text, depending on current state
+    if (emojis.style.display == 'flex') {
       // clock is visible. hide it
       emojis.style.display = 'none';
       // change button text
       showHideButton.innerHTML = 'Show';
     }
     else {
-      // clock is hidden. show it
-      emojis.style.display = 'block';
+      // emoji is hidden. show it
+      emojis.style.display = 'flex';
       // change button text
       showHideButton.innerHTML = 'Hide';
     }
